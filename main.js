4fa10d3561808e1995ab9d8ac42be2d5857b15f2
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-05-18 14:29:35 -0400

    built a counter using jquery...its broke

---
+++ main.js
@@ -0,0 +1,35 @@
+// Query elements
+const remove = $('#remove')
+const add = $('#add')
+const count = $('#count')
+const boxes = $('.boxes')
+const box = $('.box')
+
+// Set variables
+let num = 0
+
+// Set count and onClick
+count.text(num)
+
+remove.on('click', () => {
+    num--
+    count.text(num)
+    boxBuilder(num)
+})
+
+add.on('click', () => {
+    num++
+    count.text(num)
+    boxBuilder(num)
+})
+
+// Set boxes
+const boxBuilder = num => {
+    for(let i = 0; i < num; i++) {
+        const layout = $(`
+            <div class="box">*</div>
+        `)
+        boxes.append(layout)
+    }
+}
+box.remove()
